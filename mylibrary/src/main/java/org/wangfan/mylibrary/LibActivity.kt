75021f2cd405687502f8f6266f736c140f448a0b
wangfan <wangfansh@foxmail.com>
2022-07-30 00:18:12 +0800

    init

---
+++ mylibrary/src/main/java/org/wangfan/mylibrary/LibActivity.kt
@@ -0,0 +1,11 @@
+package org.wangfan.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lib)
+    }
+}
